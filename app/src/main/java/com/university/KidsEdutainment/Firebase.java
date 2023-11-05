1667ab697c27eb1a911d4ca56fd8571ac689762f
[TAEYEN02] <[xodus2331@naver.com]>
2023-11-05 12:42:23 +0900

    Amazon Polly build.gradle 설정

--- app/src/main/java/com/university/KidsEdutainment/Firebase.java
+++ app/src/main/java/com/university/KidsEdutainment/Firebase.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.koreait.edutainment.R;
+
 public class Firebase extends AppCompatActivity {
 
     @Override
