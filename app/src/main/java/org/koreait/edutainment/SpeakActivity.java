cebbbdc19836b89e6551df020fef6962a0a494d9
[TAEYEN02] <[xodus2331@naver.com]>
2023-11-10 21:34:01 +0900

    AMAZON POLLY TTS 서비스 입력(2)

--- app/src/main/java/org/koreait/edutainment/SpeakActivity.java
+++ app/src/main/java/org/koreait/edutainment/SpeakActivity.java
@@ -2,12 +2,9 @@ package org.koreait.edutainment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
-import org.koreait.edutainment.databinding.ActivitySpeakBinding;
 
 public class SpeakActivity extends AppCompatActivity {
     TextView sign;
